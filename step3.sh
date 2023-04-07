911db58b4c523836ddec55d5e0eae1b4e462d555
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-04-07 14:07:38 -0500

    Delete step3.sh

--- step3.sh
+++
@@ -1,6 +0,0 @@
-mkinitcpio -P
-pacman -S grub grub-btrfs efibootmgr base-devel linux-headers networkmanager network-manager-applet wpa_supplicant dialog os-prober     mtools dosfstools refector git
-
-pacman -S bluez bluez-utils cups xdg-utils xdg-user-dirs btrfs-progs intel-ucode OR amd-ucode
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
-blkid -o value /dev/nvme0n1p2 | head -n1 | tee -a /etc/default/grub
