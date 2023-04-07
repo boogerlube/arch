06dda9a65aee3934dbc7f6b6cd11f8ae24de21ed
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-04-07 18:40:32 -0500

    fix swap issue

--- step6.sh
+++ step6.sh
@@ -1,6 +1,10 @@
 grub-mkconfig -o /boot/grub/grub.cfg
 chattr +C /swap
-btrfs filesystem mkswapfile --size <<RAM size>>G /swap/swapfile
+read -p 'Swap size in GB? ' MEM
+MEMSIZE="$MEM""G"
+echo btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
+read -p 'press [enter] to continue'
+btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
 echo "/swap/swapfile none swap defaults 0 0" | tee -a /etc/fstab
 #passwd root
 useradd -m bob
