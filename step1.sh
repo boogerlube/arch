84ebe298c944054e13906c4c788b81cf7aa09844
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-04-06 20:32:09 -0500

    Initial commit

---
+++ step1.sh
@@ -0,0 +1,28 @@
+mkfs.fat -F32 /dev/nvme0n1p1
+cryptsetup luksFormat /dev/nvme0n1p2
+cryptsetup open /dev/nvme0n1p2 root   (/dev/mapper/root)
+mkfs.btrfs /dev/mapper/root
+mount /dev/mapper/root/mnt
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@home
+btrfs su cr /mnt/@snapshots
+btrfs su cr /mnt/@log
+btrfs su cr /mnt/@swap
+umount /mnt
+mount -o noatime,commit=120,compress=zstd,space_cache,subvol=@ /dev/mapper/root /mnt
+mount -m -o noatime /dev/nvme0n1p1 /mnt/boot
+mount -m -o noatime,commit=120,compress=zstd,space_cache,subvol=@home /dev/mapper/root /mnt/home
+mount -m -o noatime,commit=120,compress=zstd,space_cache,subvol=@log /dev/mapper/root /mnt/var/log
+mount -m -o noatime,commit=120,compress=zstd,space_cache,subvol=@snapshots /dev/mapper/root /mnt/.snapshots
+mount -m -o noatime,commit=120,compress=zstd,space_cache,subvol=@swap /dev/mapper/root /mnt/swap
+reflector -c us -f 20 -l 15 --protocol https --save /etc/pacman.d/mirrorlist
+pacstrap -K /mnt base base-devel linux linux-firmware nano dhclient
+genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab     (make sure it looks ok)
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+hwclock --systohc
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "<<machine name>>" > /etc/hostname
---
+++ step6.sh
@@ -0,0 +1,17 @@
+sudo pacman -S cinnamon lightdm-gtk-greeter firefox terminator
+sudo systemctl enable lightdm
+sudo systemctl enable NetworkManager
+sudo systemctl enable cups.service
+
+cd ~
+git clone https://aur.archlinux.org/yay.git
+git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+sudo echo '[chaotic-aur]' | tee -a /etc/pacman.conf
+sudo echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | tee -a /etc/pacman.conf
+sed -i 's/#[multilib]/[multilib]/' /etc/pacman.conf
+sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/' /etc/pacman.conf
