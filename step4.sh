1c92eb613c894eff3d742d38b9d8ed42e501128a
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-04-07 19:44:05 -0500

    fixed grub cmdline

--- step4.sh
+++ step4.sh
@@ -5,4 +5,9 @@ mkinitcpio -P
 pacman -S --noconfirm grub grub-btrfs efibootmgr base-devel linux-headers networkmanager network-manager-applet wpa_supplicant dialog os-prober mtools dosfstools reflector git bluez bluez-utils cups xdg-utils xdg-user-dirs btrfs-progs intel-ucode
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
-blkid -o value /dev/nvme0n1p2 | head -n1 | tee -a /etc/default/grub
+#blkid -o value /dev/nvme0n1p2 | head -n1 | tee -a /etc/default/grub
+blkid -o value /dev/nvme0n1p2 | head -n1 > UUID.tmp
+UUID=$(cat UUID.tmp)
+CMD='cryptdevice=UUID='$UUID':root:allow-discards root=/dev/mapper/root '
+echo -e '\n'$CMD | tee -a /etc/default/grub
+rm UUID.tmp
