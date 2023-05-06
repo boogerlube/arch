e797ee5303a6b96ed1a4ec082b939fb9306e4439
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-05-06 15:18:33 -0500

    fix file copy

--- step6.sh
+++ step6.sh
@@ -2,15 +2,12 @@ grub-mkconfig -o /boot/grub/grub.cfg
 chattr +C /swap
 read -p 'Swap size in GB? ' MEM
 MEMSIZE="$MEM""G"
-#echo btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
-#read -p 'press [enter] to continue'
+echo btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
+read -p 'press [enter] to continue'
 btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
 echo "/swap/swapfile none swap defaults 0 0" | tee -a /etc/fstab
 #passwd root
 useradd -m bob
-passwd bob
-useradd aurbuilder --system
-# create file bob in /etc/sudoers.d
+create file bob in /etc/sudoers.d
 echo "bob ALL=(ALL) ALL" >> /etc/sudoers.d/bob
-echo "aurbuilder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/aurbuilder
-echo "root ALL=(aurbuilder) NOPASSWD: ALL" >> /etc/sudoers.d/aurbuilder
+cp /root/step7.sh /home/bob/
