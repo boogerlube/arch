d1a29f637d9c009afcb4d1ac7ac6786d653d317c
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-05-09 15:59:15 -0500

    update steps

--- step4.sh
+++ step4.sh
@@ -2,7 +2,7 @@ mkinitcpio -P
 
 # edit the following line for your specific CPU type (intel-ucode OR amd-ucode)
 
-pacman -S --noconfirm grub grub-btrfs efibootmgr base-devel linux-headers networkmanager network-manager-applet wpa_supplicant dialog os-prober mtools dosfstools reflector git bluez bluez-utils cups xdg-utils xdg-user-dirs btrfs-progs intel-ucode
+pacman -S --noconfirm grub grub-btrfs efibootmgr base-devel linux-headers networkmanager network-manager-applet wpa_supplicant dialog os-prober mtools dosfstools reflector git bluez bluez-utils cups xdg-utils xdg-user-dirs btrfs-progs intel-ucode nfs-utils
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
 #blkid -o value /dev/nvme0n1p2 | head -n1 | tee -a /etc/default/grub
