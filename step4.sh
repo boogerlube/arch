4d5440ab5115a4164a6cdf42b3d8bbe677d9660a
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-05-16 14:16:26 -0500

    amd or intel ucode

--- step4.sh
+++ step4.sh
@@ -2,7 +2,12 @@ mkinitcpio -P
 
 # edit the following line for your specific CPU type (intel-ucode OR amd-ucode)
 
-pacman -S --noconfirm grub grub-btrfs efibootmgr base-devel linux-headers networkmanager network-manager-applet wpa_supplicant dialog os-prober mtools dosfstools reflector git bluez bluez-utils cups xdg-utils xdg-user-dirs btrfs-progs intel-ucode nfs-utils
+pacman -S --noconfirm grub grub-btrfs efibootmgr base-devel linux-headers networkmanager network-manager-applet wpa_supplicant dialog os-prober mtools dosfstools reflector git bluez bluez-utils usbutils cups xdg-utils xdg-user-dirs btrfs-progs
+
+#uncomment amd or intel as necessary for the current machine
+
+#pacman -S amd-ucode
+#pacman -S intel-ucode
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
 #blkid -o value /dev/nvme0n1p2 | head -n1 | tee -a /etc/default/grub
