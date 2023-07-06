0a6f31ddc4fa93cebf76dcbc4fc3ab4d9cc602db
boogerlube <85855225+boogerlube@users.noreply.github.com>
2023-07-05 21:08:05 -0500

    added arch keyring fixes

--- step2.sh
+++ step2.sh
@@ -3,3 +3,6 @@ hwclock --systohc
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+rm -rf /etc/pacman.d/gnupg
+pacman-key --init
+pacman-key --populate archlinux
