5b8f696c168dd020c1d33b6fa3c4272cc6517bd0
boogerlube <bob@languy.com>
2023-08-13 14:50:40 -0500

    streamlined script

--- step2.sh
+++ step2.sh
@@ -6,7 +6,43 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 rm -rf /etc/pacman.d/gnupg
 pacman-key --init
 pacman-key --populate archlinux
-echo -e ""
-echo -e ""
-cat step3.txt
-echo -e ""
+sed -i '/^HOOKS=/ s/filesystems/encrypt &/g' /etc/mkinitcpio.conf
+read -p 'Hostname? ' HOST
+echo $HOST > /etc/hostname
+mkinitcpio -P
+
+# edit the following line for your specific CPU type (intel-ucode OR amd-ucode)
+
+pacman -S --noconfirm grub grub-btrfs efibootmgr base-devel linux-headers networkmanager network-manager-applet wpa_supplicant 
+pacman -S --noconfirm dialog os-prober mtools dosfstools reflector git bluez bluez-utils usbutils cups xdg-utils xdg-user-dirs btrfs-progs
+
+ucode=$(lscpu | grep "^Vendor ID:" | awk -F":" '{print $2}' | xargs)
+
+if [[ "$ucode" == *"Intel"* ]]; then
+  echo "Intel processor detected. Installing intel-ucode...."
+  pacman -S --noconfirm intel-ucode
+elif [[ "$ucode" == *"AMD"* ]]; then
+  echo "AMD processor detected. Installing amd-ucode...."
+  pacman -S --noconfirm amd-ucode
+else
+  echo "No Intel or AMD processor detected."
+fi
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+UUID=$(blkid -o value /dev/nvme0n1p2 | head -n1)
+CMD='cryptdevice=UUID='$UUID':root:allow-discards root=/dev/mapper/root '
+sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT/ s/loglevel=3/$CMD &/g" /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+chattr +C /swap
+read -p 'Swap size in GB? ' MEM
+MEMSIZE="$MEM""G"
+echo btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
+read -p 'press [enter] to continue'
+btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
+echo "/swap/swapfile none swap defaults 0 0" | tee -a /etc/fstab
+#passwd root
+useradd -m bob
+passwd bob
+#create file bob in /etc/sudoers.d
+echo "bob ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/bob
+cp /root/step7.sh /home/bob/
