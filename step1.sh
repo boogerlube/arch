708c4b9f3e1ceb0e41e8e9cf75e0a62514d97c8b
boogerlube <bob@languy.com>
2023-08-17 15:29:06 -0500

    Added comments and clean up

--- step1.sh
+++ step1.sh
@@ -1,6 +1,16 @@
+######
+##      fix the disk variable otherwise you are gonna have a bad time!
+######
+
 export disk="/dev/nvme0n1"
 export diskpart="/dev/nvme0n1p"
+
+# Set the time
+
 timedatectl set-ntp true
+
+# Wipe and partition disks
+
 wipefs -af $disk
 sgdisk --zap-all --clear $disk
 partprobe $disk
@@ -8,8 +18,14 @@ sgdisk -n 0:0:+512MiB -t 0:ef00 -c 0:esp $disk
 sgdisk -n 0:0:0 -t 0:8309 -c 0:luks $disk
 partprobe $disk
 mkfs.vfat -F32 -n ESP ${diskpart}1
+
+# Setup encryption
+
 cryptsetup luksFormat ${diskpart}2
 cryptsetup open ${diskpart}2 root
+
+# Make and mount filesystems setup btrfs subvolumes
+
 mkfs.btrfs -L archlinux /dev/mapper/root
 mount /dev/mapper/root /mnt
 btrfs su cr /mnt/@
@@ -21,6 +37,9 @@ btrfs su cr /mnt/@cache
 btrfs su cr /mnt/@libvirt
 btrfs su cr /mnt/@tmp
 umount /mnt
+
+# Set your subvolume options here
+
 export sv_opts="rw,noatime,commit=120,compress-force=zstd:1,space_cache=v2"
 mount -o ${sv_opts},subvol=@ /dev/mapper/root /mnt
 mount -m -o noatime ${diskpart}1 /mnt/boot
@@ -31,12 +50,33 @@ mount -m -o ${sv_opts},subvol=@swap /dev/mapper/root /mnt/swap
 mount -m -o ${sv_opts},subvol=@cache /dev/mapper/root /mnt/var/cache
 mount -m -o ${sv_opts},subvol=@libvirt /dev/mapper/root /mnt/var/lib/libvirt
 mount -m -o ${sv_opts},subvol=@tmp /dev/mapper/root /mnt/var/tmp
+
+# Find the best mirrors for installation
+
 reflector -c us -f 20 -l 15 --protocol https --save /etc/pacman.d/mirrorlist
+
+# Finally! Install the base system
+
 pacstrap -K /mnt base base-devel linux linux-firmware nano dhclient
-#For LTS kernel comment out line above and uncomment line below
+
+# For LTS kernel comment out line above and uncomment line below:
 #pacstrap -K /mnt base base-devel linux-lts linux-firmware nano dhclient
+
+# Create the fstab table and save it
+
 genfstab -U /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
+
+# Copy the rest of the installer to the new root filesystem
+
 cp /root/arch/* /mnt/root/
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/
+
+Enter the new root filesystem to continue configuration
+
+echo -e ""
+echo -e ""
+echo "please cd to the /root directory and run step2.sh"
+echo -e ""
+
 arch-chroot /mnt
--- step2.sh
+++ step2.sh
@@ -1,22 +1,36 @@
+######
+##      fix the disk variable otherwise you are gonna have a bad time!
+######
 export disk="/dev/nvme0n1p"
+
+# Setup timezone and locale
+
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock --systohc
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# setup pacman keys
+
 rm -rf /etc/pacman.d/gnupg
 pacman-key --init
 pacman-key --populate archlinux
+
+# Add encryption to initramfs and setup hostname
+
 sed -i '/^HOOKS=/ s/filesystems/encrypt &/g' /etc/mkinitcpio.conf
 read -p 'Hostname? ' HOST
 echo $HOST > /etc/hostname
 mkinitcpio -P
 
-# edit the following line for your specific CPU type (intel-ucode OR amd-ucode)
+# Setup necessary tools
 
-pacman -S --noconfirm grub grub-btrfs efibootmgr base-devel linux-headers networkmanager network-manager-applet wpa_supplicant 
+pacman -S --noconfirm grub grub-btrfs efibootmgr base-devel linux-headers networkmanager network-manager-applet wpa_supplicant iwd
 pacman -S --noconfirm dialog os-prober mtools dosfstools reflector git bluez bluez-utils usbutils cups xdg-utils xdg-user-dirs btrfs-progs
 pacman -S --noconfirm bash-completion cryptsetup man-db pacman-contrib
+
+# Add CPU microcode to system
 
 ucode=$(lscpu | grep "^Vendor ID:" | awk -F":" '{print $2}' | xargs)
 
@@ -30,21 +44,35 @@ else
   echo "No Intel or AMD processor detected."
 fi
 
+# Install grub and configure it for encryption
+
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
 UUID=$(blkid -o value ${disk}2 | head -n1)
 CMD='cryptdevice=UUID='$UUID':root:allow-discards root=/dev/mapper/root'
 sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s|loglevel=3|$CMD &|g" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
+
+#  Setup swap
+
 chattr +C /swap
 read -p 'Swap size in GB? ' MEM
 MEMSIZE="$MEM""G"
 echo btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
-read -p 'press [enter] to continue'
 btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
 echo "/swap/swapfile none swap defaults 0 0" | tee -a /etc/fstab
+
+#    Add user
+
 #passwd root
 useradd -m bob
 passwd bob
 #create file bob in /etc/sudoers.d
 echo "bob ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/bob
+
+#  copy last step to user directory 'cause we gotta reboot!
+
 cp /root/step3.sh /home/bob/
+
+echo -e ""
+echo -e ""
+echo "Please reboot now"
