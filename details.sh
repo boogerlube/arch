b2682ef4da164b45b0c5550ff9b0e3c5ab266a18
boogerlube <bob@languy.com>
2023-09-26 08:58:26 -0500

    merged sounds

--- details.sh
+++ details.sh
@@ -1,12 +1,18 @@
+
+# Make yerself t'home!
+
 tar -xvf post-install.tar.gz
 cp -r Wallpapers ~/Pictures/
 cp ./arch-shell/.* ~
 cat fstab.txt | sudo tee -a /etc/fstab
 sudo mkdir /media
-sudo mkdir /media/pinky
-sudo mkdir /media/share
-sudo mkdir /media/torrent
+sudo mkdir -p /media/{pinky,share,torrent}
 sudo mount -a
 source ~/.bashrc
 sudo mkdir /etc/samba
 sudo cp smb.conf /etc/samba/
+mkdir ~/.local/bin
+cp scripts/* ~/.local/.bin
+chmod +x ~/.local/bin/*.sh
+sudo cp ./sounds/* /usr/share/sounds/
+dconf load /org/cinnamon/sounds/ < sounds.txt
