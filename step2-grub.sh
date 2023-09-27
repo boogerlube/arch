dd24846a48f1656e446287377ac691de3aff9a21
boogerlube <bob@languy.com>
2023-09-27 16:18:27 -0500

    fixed step2

--- step2-grub.sh
+++ step2-grub.sh
@@ -61,7 +61,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 chattr +C /swap
 read -p 'Swap size in GB? ' MEM
 MEMSIZE="$MEM""G"
-echo btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
 btrfs filesystem mkswapfile --size $MEMSIZE /swap/swapfile
 echo "/swap/swapfile none swap defaults 0 0" | tee -a /etc/fstab
 
