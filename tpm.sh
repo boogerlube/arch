2433c87b15012bfdeb4d686f70ef1a7ae05ba61f
boogerlube <bob@languy.com>
2023-10-14 15:37:24 -0500

    updated tpm

--- tpm.sh
+++ tpm.sh
@@ -1,2 +1,5 @@
-sudo systemd-cryptenroll /dev/gpt-auto-root-luks --recovery-key
+
+echo -e "Recovery key for $(hostname) generated on $(date)\n" > $(hostname)-recovery.txt
+
+sudo systemd-cryptenroll /dev/gpt-auto-root-luks --recovery-key | tee -a $(hostname)-recovery.txt
 sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+7  --tpm2-with-pin=yes /dev/gpt-auto-root-luks
