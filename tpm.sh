ff6f5bf444b5ec0d3dfddb552fb0c7cb1992c1d9
boogerlube <bob@languy.com>
2023-10-14 16:09:12 -0500

    updated tpm

--- tpm.sh
+++ tpm.sh
@@ -1,5 +1,21 @@
+#############################################################################
+###           Be sure and update disk before running this script          ###
+### Usual options are:                                                    ###
+### /dev/gpt-auto-root-luks for DPS                                       ###
+### /dev/sda2 for ProxMox                                                 ### 
+### /dev/nvme0n1p2 for physical machines                                  ###
+#############################################################################
 
-echo -e "Recovery key for $(hostname) generated on $(date)\n" > $(hostname)-recovery.txt
+disk="/dev/gpt-auto-root-luks"
 
-sudo systemd-cryptenroll /dev/gpt-auto-root-luks --recovery-key | tee -a $(hostname)-recovery.txt
-sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+7  --tpm2-with-pin=yes /dev/gpt-auto-root-luks
+if ! [ -e $disk ] ; then
+   echo -e "\n\nDevice does not exist!"
+   exit
+fi
+
+echo -e "Recovery key for $(hostname) generated on $(date).\n" > $(hostname)-recovery.txt
+
+sudo systemd-cryptenroll $disk --recovery-key | tee -a $(hostname)-recovery.txt
+sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=0+7  --tpm2-with-pin=yes $disk
+
+echo -e "\n\n" >> $(hostname)-recovery.txt
