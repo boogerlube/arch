b5b4019fa7391e52ab1da392150b41353be023f9
boogerlube <bob@languy.com>
2023-11-26 17:07:40 -0600

    updated repair

--- repair.sh
+++ repair.sh
@@ -1,14 +1,30 @@
 export sv_opts="rw,noatime,commit=120,compress-force=zstd:1,space_cache=v2"
-disk="nvme0n1p"
+export disk="nvme0n1"
+export rootmnt="/mnt"
+
+# Make sure disk device exists before beginning
+if ! [ -e $disk ] ; then
+   echo -ne "\nDevice does not exist!\n"
+   lsblk -dpnoNAME | grep -P "/dev/sd|nvme|vd"
+   exit 1
+fi
+
+# setup partition vars
+disk="${disk,,}"
+if [[ $disk == *"nvme"* ]]; then
+  export diskroot=$disk"p2"
+  export diskboot=$disk"p1"
+else
+   export diskroot=$disk"2"
+   export diskboot=$disk"1"
+ fi
 
 #Open up encrypted root partition
-cryptsetup open /dev/{$disk}2 root
+cryptsetup open /dev/$diskroot root
 
-#mount all subvols and boot partition
-
-
+# mount all subvols and boot partition
 mount -o ${sv_opts},subvol=@ /dev/mapper/root /mnt
-mount -m -o noatime,uid=0,gid=0,fmask=0077,dmask=0077 ${disk}1 /mnt/boot
+mount -m -o noatime,uid=0,gid=0,fmask=0077,dmask=0077 $diskboot /mnt/boot
 mount -m -o ${sv_opts},subvol=@home /dev/mapper/root /mnt/home
 mount -m -o ${sv_opts},subvol=@log /dev/mapper/root /mnt/var/log
 mount -m -o ${sv_opts},subvol=@snapshots /dev/mapper/root /mnt/.snapshots
@@ -18,5 +34,10 @@ mount -m -o ${sv_opts},subvol=@libvirt /dev/mapper/root /mnt/var/lib/libvirt
 mount -m -o ${sv_opts},subvol=@tmp /dev/mapper/root /mnt/var/tmp
 
 #start chroot to begin repair
+# arch-chroot /mnt
 
-arch-chroot /mnt
+# for grub repair use:
+# arch-chroot "$rootmnt" grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+
+# for systemd boot use:
+# bootctl --path="$rootmnt"/boot install
