7ef3c4e8a8d9477bec6eeaf0472ea7f171d305fa
boogerlube <bob@languy.com>
2023-12-07 18:49:35 -0600

    fixed qemu bug

--- qemu.sh
+++ qemu.sh
@@ -5,13 +5,15 @@ read yn
 
 # install all required packages.
 sudo pacman -S --needed --noconfirm qemu-full virt-manager dnsmasq dmidecode
-sudo usermod -aG libvir $USER
+sudo usermod -aG libvirt $USER
 sudo systemctl enable --now libvirtd.service
 
+# get active connection
+CONNECTION=$(nmcli -t con show --active | awk -F":" 'NR==1{print $1}')
 
 # create a bridge interface. Be sure and set the interface correctly.
-sudo nmcli con add type bridge ifname br0 con-name br0
+sudo nmcli con add type bridge ifname br0 con-name br0 stp no
 sudo nmcli con add type bridge-slave ifname $IFACE master br0
-sudo nmcli con modify br0 bridge.stp no
-sudo nmcli con down "Auto Ethernet"
+#sudo nmcli con modify br0 bridge.stp no
+sudo nmcli con down $CONNECTION
 sudo nmcli con up br0
