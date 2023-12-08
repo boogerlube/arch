ceef71be4076bf1a070a7030d41eca2a205edd18
boogerlube <bob@languy.com>
2023-12-07 18:10:30 -0600

    update qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -10,6 +10,8 @@ sudo systemctl enable --now libvirtd.service
 
 
 # create a bridge interface. Be sure and set the interface correctly.
-sudo nmcli con add type bridge ifname br0
+sudo nmcli con add type bridge ifname br0 con-name br0
 sudo nmcli con add type bridge-slave ifname $iface master br0
+sudo nmcli con modify br0 bridge.stp no
+sudo nmcli con down "Auto Ethernet"
 sudo nmcli con up br0
