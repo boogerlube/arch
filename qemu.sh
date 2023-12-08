94f4686675f55d4a22c45a944765846d4e698b91
boogerlube <bob@languy.com>
2023-12-08 11:04:01 -0600

    updated qemu again

--- qemu.sh
+++ qemu.sh
@@ -1,4 +1,10 @@
-IFACE="enp3s0"
+#IFACE="enp3s0"
+
+# get active connection
+CONNECTION=$(nmcli -t con show --active | awk -F":" 'NR==1{print $1}')
+IFACE=$(nmcli -t con show --active | awk -F":" 'NR==1{print $4}')
+
+echo -ne "\n Interface "$IFACE" is on connection "$CONNECTION".\n"
 
 echo -ne "\n Did you set the interface?"
 read yn
@@ -8,8 +14,7 @@ sudo pacman -S --needed --noconfirm qemu-full virt-manager dnsmasq dmidecode
 sudo usermod -aG libvirt $USER
 sudo systemctl enable --now libvirtd.service
 
-# get active connection
-CONNECTION=$(nmcli -t con show --active | awk -F":" 'NR==1{print $1}')
+
 
 # create a bridge interface. Be sure and set the interface correctly.
 sudo nmcli con add type bridge ifname br0 con-name br0 stp no
