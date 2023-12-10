061bf5c074d5bb751024aa5780d7622cd4ad9f5c
boogerlube <bob@languy.com>
2023-12-09 21:00:40 -0600

    updated scripts and aliases

--- qemu.sh
+++ qemu.sh
@@ -10,13 +10,20 @@ echo -ne "\nAre the interface and connection correct?"
 read yn
 
 # install all required packages.
-sudo pacman -S --needed --noconfirm qemu-full virt-manager dnsmasq dmidecode
+sudo pacman -S --needed --noconfirm qemu-full virt-manager dnsmasq dmidecode swtpm
 sudo usermod -aG libvirt $USER
 sudo systemctl enable --now libvirtd.service
 
 # create a bridge interface. Be sure and set the interface correctly.
 sudo nmcli con add type bridge ifname br0 con-name br0 stp no
 sudo nmcli con add type bridge-slave ifname $IFACE master br0
-#sudo nmcli con modify br0 bridge.stp no
 sudo nmcli con down "$CONNECTION"
 sudo nmcli con up br0
+sudo nmcli con up bridge-slave-$IFACE
+#sudo nmcli con modify br0 bridge.stp no
+
+# test qemu installation
+sudo virt-host-validate qemu
+
+# use <shift><F12> to release mouse pointer and <shift><F11> to switch full screen mode
+# use <L-CTRL><L-ALT> to release mouse as well.
