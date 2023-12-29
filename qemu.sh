82626b6568362a496962d7ee4c94265c0fed536a
boogerlube <bob@languy.com>
2023-12-28 18:43:46 -0600

    updated QEMU

--- qemu.sh
+++ qemu.sh
@@ -1,26 +1,25 @@
-#IFACE="enp3s0"
-
-# get active connection
-CONNECTION=$(nmcli -t con show --active | awk -F":" 'NR==1{print $1}')
-IFACE=$(nmcli -t con show --active | awk -F":" 'NR==1{print $4}')
-
-echo -ne "\n Interface "$IFACE" is on connection "$CONNECTION".\n"
-
-echo -ne "\nAre the interface and connection correct?"
-read yn
+BRIDGE=false
 
 # install all required packages.
 sudo pacman -S --needed --noconfirm qemu-full virt-manager dnsmasq dmidecode swtpm
 sudo usermod -aG libvirt $USER
 sudo systemctl enable --now libvirtd.service
 
-# create a bridge interface. Be sure and set the interface correctly.
-sudo nmcli con add type bridge ifname br0 con-name br0 stp no
-sudo nmcli con add type bridge-slave ifname $IFACE master br0
-sudo nmcli con down "$CONNECTION"
-sudo nmcli con up br0
-sudo nmcli con up bridge-slave-$IFACE
-#sudo nmcli con modify br0 bridge.stp no
+if $BRIDGE ; then 
+  # create a bridge interface. Be sure and set the interface correctly.
+  # get active connection
+  CONNECTION=$(nmcli -t con show --active | awk -F":" 'NR==1{print $1}')
+  IFACE=$(nmcli -t con show --active | awk -F":" 'NR==1{print $4}')
+  echo -ne "\n Interface "$IFACE" is on connection "$CONNECTION".\n"
+  echo -ne "\nAre the interface and connection correct?"
+  read yn
+  sudo nmcli con add type bridge ifname br0 con-name br0 stp no
+  sudo nmcli con add type bridge-slave ifname $IFACE master br0
+  sudo nmcli con down "$CONNECTION"
+  sudo nmcli con up br0
+  sudo nmcli con up bridge-slave-$IFACE
+  #sudo nmcli con modify br0 bridge.stp no
+fi
 
 echo -ne "\nReady to confirm installation?"
 read yn
